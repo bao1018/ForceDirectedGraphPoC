d9af60ffa1b83e7d85f36b1750d6ac86bf9910df
Herbert Li <loveltyoic@gmail.com>
2014-02-11 11:31:13 +0800

    When click a node, move it to center.

--- arborjs/main.js
+++ arborjs/main.js
@@ -43,20 +43,21 @@
         }
       },
       resize:function(){
-        canvas.width = $(window).width()
-        canvas.height = .75* $(window).height()
+        // canvas.width = $(window).width()
+        // canvas.height = .75* $(window).height()
         sys.screen({size:{width:canvas.width, height:canvas.height}})
         _vignette = null
         that.redraw()
       },
       redraw:function(){
         gfx.clear()
+        gfx.background("#FFF8D7");
         sys.eachEdge(function(edge, p1, p2){
           if (edge.source.data.alpha * edge.target.data.alpha == 0) return
           gfx.line(p1, p2, {stroke:"#b2b19d", width:2, alpha:edge.target.data.alpha})
         })
         sys.eachNode(function(node, pt){
-          var w = Math.max(30, 30)
+          var w = node.data.radius || Math.max(30, 30)
           if (node.data.alpha===0) return
           gfx.oval(pt.x-w/2, pt.y-w/2, w, w, {fill:node.data.color, alpha:node.data.alpha})
           gfx.text(node.name, pt.x+50, pt.y, {color:"black", align:"center", font:"Arial", size:12})
@@ -84,14 +85,13 @@
 
           _vignette = {top:top, bot:bot}
         }
-        
-        // top
-        ctx.fillStyle = _vignette.top
-        ctx.fillRect(0,0, w,r)
+        // // top
+        // ctx.fillStyle = _vignette.top
+        // ctx.fillRect(0,0, w,r)
 
-        // bot
-        ctx.fillStyle = _vignette.bot
-        ctx.fillRect(0,h-r, w,r)
+        // // bot
+        // ctx.fillStyle = _vignette.bot
+        // ctx.fillRect(0,h-r, w,r)
       },
 
       switchMode:function(e){
@@ -122,7 +122,6 @@
           related.push (edge.source)
         });
         related.push(selected);
-        
         sys.eachNode(function(node){
           // if (node.data.shape=='dot') return // skip all but leafnodes
           var nowVisible = ($.inArray(node, related)>=0)
@@ -182,23 +181,27 @@
             _mouseP = arbor.Point(e.pageX-pos.left, e.pageY-pos.top)
             nearest = dragged = sys.nearest(_mouseP);
             
-            if (nearest && selected && nearest.node===selected.node){
-              var link = selected.node.data.link
-              if (link.match(/^#/)){
-                 $(that).trigger({type:"navigate", path:link.substr(1)})
-              }else{
-                 window.location = link
-              }
-              return false
-            }
+            // if (nearest && selected && nearest.node===selected.node){
+            //   var link = selected.node.data.link
+            //   if (link.match(/^#/)){
+            //      $(that).trigger({type:"navigate", path:link.substr(1)})
+            //   }else{
+            //      window.location = link
+            //   }
+            //   return false
+            // }
             if (nearest.node) {
-              _section = nearest.node.name
-              that.switchSection(_section)
+              var _section = nearest.node.name;
+              var pos = $(canvas).offset();
+              var center = arbor.Point(canvas.width/2+pos.left, canvas.height/2+pos.top);
+              var p = sys.fromScreen(center);
+              nearest.node.p = p;
+              that.switchSection(_section);
             }
             
             if (dragged && dragged.node !== null) dragged.node.fixed = true
 
-            $(canvas).unbind('mousemove', handler.moved);
+            // $(canvas).unbind('mousemove', handler.moved);
             $(canvas).bind('mousemove', handler.dragged)
             $(window).bind('mouseup', handler.dropped)
 
@@ -353,7 +356,7 @@
     }
 
     var theUI = {
-      nodes:{"arbor.js":{color:"red", shape:"dot", alpha:1}, 
+      nodes:{"arbor.js":{color:"red", shape:"dot", alpha:1, radius: 30}, 
       
              demos:{color:CLR.branch, shape:"dot", alpha:1}, 
              halfviz:{color:CLR.demo, alpha:0, link:'/halfviz'},
@@ -396,7 +399,7 @@
     sys.graft(theUI)
     
     var nav = Nav("#nav")
-    $(sys.renderer).bind('navigate', nav.navigate)
+    // $(sys.renderer).bind('navigate', nav.navigate)
     $(nav).bind('mode', sys.renderer.switchMode)
     nav.init()
   })
